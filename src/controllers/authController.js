330f73826712e5d3340e8f3c6f6485872b2ad225
srinjay-yactraq <srinjay@yactraq.com>
2025-10-18 04:53:59 +0530

    sameSite problem solved

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -37,7 +37,7 @@ exports.login = async (req, res) => {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       maxAge: 60 * 60 * 1000,
-      sameSite: 'strict',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
     });
 
    
@@ -62,7 +62,7 @@ exports.logout = async (req, res) => {
     res.clearCookie('token', {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
     });
 
     res.json({ message: 'Logged out successfully, token blacklisted' });
