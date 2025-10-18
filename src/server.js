d904d6a17e005f27a992c614e32880e5fd7df7b7
srinjay-yactraq <srinjay@yactraq.com>
2025-10-18 06:10:13 +0530

    reverting cahnges

--- src/server.js
+++ src/server.js
@@ -11,13 +11,6 @@ connectDB()
 
 const app = express();
 
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL);
-//   res.header("Access-Control-Allow-Credentials", "true");
-//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-//   next();
-// });
 
 app.use(cors({
   origin: process.env.CLIENT_URL || 'http://localhost:3001',
